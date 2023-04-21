85739c29f44129454a292194f8738a5b033c06e9
Renato Gualberto <gprenatofreitas@gmail.com>
2023-04-21 12:00:45 -0300

    criação da classe
    criando a primeira classe

--- app_BackEnd_PUC/app_BackEnd_PUC/Models/veiculo.cs
+++ app_BackEnd_PUC/app_BackEnd_PUC/Models/veiculo.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace app_BackEnd_PUC.Models
 {
+    [Table("veiculos")]
     public class veiculo
     {
+        [key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
     }
 }
